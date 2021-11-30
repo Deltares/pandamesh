44e478a272972e05daee9c386171b6c76cc653ad
Huite <Huite@users.noreply.github.com>
2021-11-30 14:00:24 +0000

    deploy: 2ef2beac589a1e27a674bfbe72e82f8a510e1b01

---
+++ _downloads/901731ce545f172d35561b334a49dd37/triangle-basic.py
@@ -0,0 +1,180 @@
+"""
+Basic Triangle Example
+======================
+
+In this example we'll create some basic geometries and turn them into meshes.
+to illustrate some of the mesh generation features that Triangle provides in
+combination with polygon, point, and linestring geometries represented by
+geopandas.
+"""
+# %%
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import numpy as np
+import shapely.geometry as sg
+
+import pandamesh as pm
+
+# %%
+# A simple rectangular mesh
+# -------------------------
+#
+# The most simple example is perhaps a rectangle. We'll create a vector
+# geometry, store this in a geodataframe, and associate a cell size.
+
+polygon = sg.Polygon(
+    [
+        [0.0, 0.0],
+        [10.0, 0.0],
+        [10.0, 10.0],
+        [0.0, 10.0],
+    ]
+)
+gdf = gpd.GeoDataFrame(geometry=[polygon])
+gdf["cellsize"] = 2.0
+
+# %%
+# We'll use this polygon to generate a mesh. We start by initializing a
+# TriangleMesher, which is a simple wrapper around the Python bindings to the
+# Triangle C-library. This wrapper extracts the coordinates and presents them
+# in the appropriate manner for triangle.
+
+mesher = pm.TriangleMesher(gdf)
+vertices, triangles = mesher.generate()
+pm.plot(vertices, triangles)
+
+# %%
+# Defaults
+# --------
+#
+# The TriangleMesher class is initialized with a number of default parameters:
+
+print(mesher)
+
+# %%
+# We can change a parameter, and see what effects this has on the mesh:
+
+mesher.conforming_delaunay = False
+vertices, triangles = mesher.generate()
+pm.plot(vertices, triangles)
+
+# %%
+# To generate a mesh with smaller cell sizes, we adjust the geodataframe, and
+# recreate the mesher.
+
+gdf["cellsize"] = 1.0
+mesher = pm.TriangleMesher(gdf)
+vertices, triangles = mesher.generate()
+pm.plot(vertices, triangles)
+# %%
+# Multiple cell size zones
+# ------------------------
+#
+# Multiple zones of cell sizes are supported, as every polygon can be associated
+# with a cell size in the geodataframe.
+
+polygon2 = sg.Polygon(
+    [
+        [10.0, 0.0],
+        [20.0, 0.0],
+        [20.0, 10.0],
+        [10.0, 10.0],
+    ]
+)
+gdf = gpd.GeoDataFrame(geometry=[polygon, polygon2])
+gdf["cellsize"] = [2.0, 1.0]
+
+mesher = pm.TriangleMesher(gdf)
+vertices, triangles = mesher.generate()
+pm.plot(vertices, triangles)
+# %%
+# Polygons with holes ("donut" geometries)
+# ----------------------------------------
+#
+# Holes in polygons work as expected:
+
+outer = [(0.0, 0.0), (10.0, 0.0), (10.0, 10.0), (0.0, 10.0)]
+inner = [(3.0, 3.0), (7.0, 3.0), (7.0, 7.0), (3.0, 7.0)]
+
+donut = sg.Polygon(shell=outer, holes=[inner])
+gdf = gpd.GeoDataFrame(geometry=[donut])
+gdf["cellsize"] = [2.0]
+
+mesher = pm.TriangleMesher(gdf)
+vertices, triangles = mesher.generate()
+pm.plot(vertices, triangles)
+
+# %%
+# Local refinement
+# ----------------
+#
+# To do local refinement, we need to ensure there is no overlap between the
+# polygons. The coordinates of the hole of the outer polygon should match
+# exactly with the coordinates of the exterior boundary of the inner polygon.
+
+refined = sg.Polygon(inner)
+
+gdf = gpd.GeoDataFrame(geometry=[donut, refined])
+gdf["cellsize"] = [2.0, 0.5]
+
+mesher = pm.TriangleMesher(gdf)
+vertices, triangles = mesher.generate()
+pm.plot(vertices, triangles)
+
+# %%
+# Force points into the triangulation
+# -----------------------------------
+#
+# We may also force points into the triangulation, by adding points to the
+# geodataframe. Let's assume we'd like to a series of points at x=1.0, at a
+# distance of 0.5.
+
+y = np.arange(0.5, 10.0, 0.5)
+x = np.full(y.size, 1.0)
+points = gpd.points_from_xy(x, y)
+
+gdf = gpd.GeoDataFrame(geometry=[donut, refined, *points])
+gdf["cellsize"] = [2.0, 0.5] + (len(points) * [np.nan])
+gdf.plot(facecolor="none")
+
+# %%
+# We can now see the points forced in the triangulation, by plotting the
+# contents of the geodataframe on top of the generated mesh:
+
+mesher = pm.TriangleMesher(gdf)
+vertices, triangles = mesher.generate()
+
+fig, ax = plt.subplots()
+pm.plot(vertices, triangles, ax=ax)
+gdf.plot(facecolor="none", edgecolor="red", ax=ax)
+# %%
+# Force linestrings into the triangulation
+# ----------------------------------------
+#
+# We may do the same with linestrings. Here, we will add a vertical line at
+# x = 9.0.
+
+line = sg.LineString(
+    [
+        [9.0, 2.0],
+        [9.0, 8.0],
+    ]
+)
+gdf = gpd.GeoDataFrame(geometry=[donut, refined, line, *points])
+gdf["cellsize"] = [2.0, 0.5, np.nan] + (len(points) * [np.nan])
+
+mesher = pm.TriangleMesher(gdf)
+vertices, triangles = mesher.generate()
+
+fig, ax = plt.subplots()
+pm.plot(vertices, triangles, ax=ax)
+gdf.plot(facecolor="none", edgecolor="red", ax=ax)
+
+# %%
+# Conclusion
+# ----------
+#
+# In real use, the vector geometries will be more complex, and not based on
+# just a few coordinate pairs. Such cases are presented in the other examples,
+# but the same principles apply: we may use polygons with associated cell
+# sizes, jand linestrings and points to steer the triangulation.
---
+++ _downloads/b09e184455098f3e19aac18afab7c2d7/triangle-geospatial.py
@@ -0,0 +1,130 @@
+"""
+Geospatial Triangle Example
+===========================
+
+In this example we'll illustrate how to generate a mesh from a "real-world"
+geospatial vector dataset.
+"""
+# %%
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import pandas as pd
+import shapely.geometry as sg
+
+import pandamesh as pm
+
+# %%
+# Overlap
+# -------
+#
+# We will open a GeoJSON file describing the provinces of the Netherlands, and
+# select only the name and geometry columns. We'll set the coordinate reference
+# system to the Dutch national standard (EPSG:28992). Finally we set the name
+# column to be used as index, so we can select provinces on name.
+
+provinces = gpd.read_file("data/provinces.geojson").loc[:, ["name", "geometry"]]
+provinces = provinces.to_crs("epsg:28992")
+provinces.index = provinces["name"]
+gdf = provinces.copy()
+
+# %%
+# The mesh generation software cannot deal with overlap of polygons. To get rid
+# of overlap, we can use the spatial functionality that geopandas provides.
+# Let's check the polygons for overlap first.
+
+overlap = gdf.overlay(gdf, how="intersection", keep_geom_type=True)
+overlap = overlap.loc[overlap["name_1"] != overlap["name_2"]]
+
+fig, ax = plt.subplots()
+gdf.plot(ax=ax)
+overlap.plot(edgecolor="red", ax=ax)
+
+# %%
+# Clean-up
+# --------
+#
+# There are many small overlaps visible at the province borders.
+#
+# We can generate a consistent polygon using a unary union.
+
+union = sg.Polygon(gdf.unary_union)
+union_gdf = gpd.GeoDataFrame(geometry=[union])
+union_gdf["cellsize"] = 10_000.0
+
+# %%
+# Unfortunately, the province boundaries of this dataset no do align neatly and
+# there are a number of small holes present. Some of these holes are not formed
+# by inconsistencies, but by a small number of Belgian exclaves,
+# `Baarle-Hertog`_.
+#
+# Simplify
+# --------
+#
+# We'll ignore the subtleties of international law for now and use geopandas to
+# remove all blemishes by:
+#
+# * squeezing out the holes with ``.buffer``
+# * dissolving the buffered polygons into a single polygon with ``.dissolve``
+# * simplifying the dissolved polygon to avoid over-refinement with ``.simplify``
+#
+# This creates a clean, and simpler, geometry.
+
+simplified = gdf.copy()
+simplified.geometry = simplified.geometry.buffer(500.0)
+simplified["dissolve_column"] = 0
+simplified = simplified.dissolve(by="dissolve_column")
+simplified.geometry = simplified.geometry.simplify(5_000.0)
+simplified["cellsize"] = 10_000.0
+
+simplified.plot()
+
+# %%
+# Using this clean geometry, we can generate an unstructured grid with a fairly
+# constant cell size.
+
+mesher = pm.TriangleMesher(simplified)
+vertices, triangles = mesher.generate()
+pm.plot(vertices, triangles)
+
+# %%
+# Local refinement
+# ----------------
+#
+# To set a zone of refinement, we can define an additional polygon. We need to
+# ensure that no overlap occurs in the follwing steps:
+#
+# * select the geometry of a single province;
+# * simplify its geometry to an appropriate level of detail;
+# * specify a smaller cell size;
+# * remove this province from the enveloping polygon;
+# * collect the two polygons in a single geodataframe.
+
+utrecht = gdf.loc[["Utrecht"]]
+utrecht.geometry = utrecht.geometry.simplify(2_500.0)
+utrecht["cellsize"] = 5000.0
+
+envelope = simplified.overlay(utrecht, how="difference")
+refined = pd.concat([envelope, utrecht])
+refined.index = [0, 1]
+refined.plot(column="name")
+
+# %%
+# This results in a mesh with a smaller cell size in the province of Utrecht.
+
+mesher = pm.TriangleMesher(refined)
+vertices, triangles = mesher.generate()
+pm.plot(vertices, triangles)
+
+# %%
+# Conclusion
+# ----------
+#
+# This example provides a taste of how to convert a geospatial vector dataset
+# into an unstructured grid with a locally refined part. Real-world data
+# generally come with their own idiosyncrasies and inconsistencies. Depending
+# on the nature of the necessary fixes, they can be solved with geopandas
+# functionality, but sometimes manual editing is required. Fortunately,
+# geopandas provides easy input and output for many file formats, which can be
+# opened by e.g. QGIS.
+#
+# .. _Baarle-Hertog: https://en.wikipedia.org/wiki/Baarle-Hertog
