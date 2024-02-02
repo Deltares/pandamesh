7ac3e79e85e65c2cd7f566403bed9a00d720879f
Huite Bootsma <huitebootsma@gmail.com>
2024-02-02 17:54:32 +0100

    Use hatchling to build

--- pandamesh/__init__.py
+++ pandamesh/__init__.py
@@ -1,5 +1,3 @@
-import pkg_resources
-
 from pandamesh import data
 from pandamesh.gmsh_mesher import (
     FieldCombination,
@@ -12,11 +10,7 @@ from pandamesh.gmsh_mesher import (
 from pandamesh.plot import plot
 from pandamesh.triangle_mesher import DelaunayAlgorithm, TriangleMesher
 
-try:
-    __version__ = pkg_resources.get_distribution(__name__).version
-except pkg_resources.DistributionNotFound:
-    # package is not installed
-    pass
+__version__ = "0.1.3"
 
 
 __all__ = (
