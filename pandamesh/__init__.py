894aef21ff5e0bc2015bca9ce5bfbe30f50180cb
JoerivanEngelen <joerivanengelen@hotmail.com>
2025-02-06 15:27:34 +0100

    Minor version increase and add python 3.13 to pyproject.toml

--- pandamesh/__init__.py
+++ pandamesh/__init__.py
@@ -12,7 +12,7 @@ from pandamesh.preprocessor import Preprocessor
 from pandamesh.triangle_enums import DelaunayAlgorithm
 from pandamesh.triangle_mesher import TriangleMesher
 
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 
 
 __all__ = (
